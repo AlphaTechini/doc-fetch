ec41ce2849eb27d83ae81e6375c81d41a493c75e
AlphaTechini <rehobothokoibu@gmail.com>
2026-02-22 18:06:56 +0100

    Sync Python version to 2.4.1 (PyPI published)

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="doc-fetch",
-    version="2.4.0",
+    version="2.4.1",
     author="AlphaTechini",
     author_email="rehobothokoibu@gmail.com",
     description="Dynamic documentation fetching CLI that converts entire documentation sites to single markdown files for AI/LLM consumption",
